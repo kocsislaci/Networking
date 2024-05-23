e907f86faf86d36f41b4ccd00fa49f2991c0546c
Imre Racz <imre@riltech.co>
2024-05-23 16:04:52 +0200

    feat(hp): bar should be lowered when hit

--- Assets/Src/BulletController.cs
+++ Assets/Src/BulletController.cs
@@ -17,6 +17,11 @@ public class BulletController : NetworkBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Hit player");
+            var ctr = other.gameObject.GetComponent<PlayerController>();
+            var hs = ctr.GetHealthSystem();
+            Debug.Log("Current hp of " + ctr.OwnerClientId + " " + hs.GetHealth());
+            hs.Damage(20);
+            Debug.Log("After hit of " + ctr.OwnerClientId + " " + hs.GetHealth());
         }
         GetComponent<NetworkObject>().Despawn();
     }
