aa67857817840226c4673218fb67a420cb136e9e
Karoly Szekely <kari1038@gmail.com>
2024-05-23 15:02:52 +0200

    change player colors

--- Assets/Src/PlayerController.cs
+++ Assets/Src/PlayerController.cs
@@ -5,30 +5,44 @@ using UnityEngine.Scripting.APIUpdating;
 public class PlayerController : NetworkBehaviour
 {
     private NetworkObject networkObject;
+    private MeshRenderer meshRenderer;
     [SerializeField] private GameObject bulletPrefab;
     private bool bulletShot = false;
 
-    public override void OnNetworkSpawn() {
+    [SerializeField] private Material[] playerSkins;
+
+    public override void OnNetworkSpawn()
+    {
         networkObject = GetComponent<NetworkObject>();
     }
 
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        SetColor((uint)OwnerClientId);
+    }
 
     private void Update()
     {
         if (!IsOwner) return;
 
         Move();
-        if (Input.GetKey(KeyCode.Space)) {
-            if (!bulletShot) {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!bulletShot)
+            {
                 AttackServerRpc();
                 bulletShot = true;
             }
-        } else {
+        }
+        else
+        {
             bulletShot = false;
         }
     }
-    
-    private void Move() {
+
+    private void Move()
+    {
         float movement = 0f;
         float movementSpeed = 5f;
         float rotation = 0f;
@@ -42,16 +56,25 @@ public class PlayerController : NetworkBehaviour
     }
 
     [ServerRpc]
-    private void MoveServerRpc(float movement, float rotation) {
+    private void MoveServerRpc(float movement, float rotation)
+    {
         transform.Translate(Vector3.forward * movement * Time.deltaTime);
         transform.RotateAround(transform.position, Vector3.up, rotation * Time.deltaTime);
     }
 
     [ServerRpc]
-    private void AttackServerRpc() {
+    private void AttackServerRpc()
+    {
         Debug.Log("Who attacks: " + OwnerClientId);
         GameObject bullet = Instantiate(bulletPrefab, transform.position + transform.forward * 1.3f + new Vector3(0, .5f, 0), transform.rotation);
         bullet.GetComponent<NetworkObject>().Spawn(true);
 
     }
+
+    void SetColor(long skinId)
+    {
+        Debug.Log($"Set Player #{OwnerClientId} color to #{skinId}");
+        meshRenderer.materials = new Material[] { playerSkins[skinId] };
+    }
+
 }
